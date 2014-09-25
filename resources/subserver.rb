3f2e3ca646b6d814070378668c2af6e220ca32f8
Julian C. Dunn <jdunn@getchef.com>
2014-09-25 01:27:02 -0400

    Remove :required => true from flags that aren't needed when :disable

--- resources/subserver.rb
+++ resources/subserver.rb
@@ -1,12 +1,12 @@
 actions :enable, :disable
 
 attribute :servicename, :name_attribute => true, :kind_of => String
-attribute :type, :kind_of => String, :required => true, values => %w(dgram stream sunrpc_udp sunrpc_tcp)
+attribute :type, :kind_of => String, :values => %w(dgram stream sunrpc_udp sunrpc_tcp)
 attribute :protocol, :kind_of => String, :required => true, :values => %w(tcp udp)
 attribute :wait, :kind_of => String, :default => 'nowait', :values => %w(wait nowait SRC)
 attribute :user, :kind_of => String, :default => 'root', :required => true
-attribute :program, :kind_of => String, :required => true
-attribute :args, :kind_of => String, :required => true
+attribute :program, :kind_of => String
+attribute :args, :kind_of => String
 
 attr_accessor :enabled
 
