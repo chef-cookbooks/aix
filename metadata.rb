7018ae4e85952518b8b677db1639efa555ed6e20
Julian C. Dunn <jdunn@getchef.com>
2014-12-09 22:36:11 -0500

    Prepare for 0.0.2 release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jdunn@getchef.com'
 license          'Apache 2.0'
 description      'Custom resources useful for AIX systems'
 long_description 'Custom resources useful for AIX systems'
-version          '0.0.1'
+version          '0.0.2'
 
 supports 'aix', '>= 6.1'
