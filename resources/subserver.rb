2d5d34d14b59be5e12e22a80408b48f81155aa15
vfoucault <vianney.foucault@gmail.com>
2015-05-19 22:42:35 +0200

    Changed attribute protocol required to false Strangly it appears that with required set to true, the recipe always claims that the attribute is non present even with the correct value

--- resources/subserver.rb
+++ resources/subserver.rb
@@ -2,7 +2,7 @@ actions :enable, :disable
 
 attribute :servicename, :name_attribute => true, :kind_of => String
 attribute :type, :kind_of => String, :equal_to => %w(dgram stream sunrpc_udp sunrpc_tcp)
-attribute :protocol, :kind_of => String, :required => true, :equal_to => %w(tcp udp tcp6 udp6)
+attribute :protocol, :kind_of => String, :required => false, :equal_to => %w(tcp udp tcp6 udp6)
 attribute :wait, :kind_of => String, :default => 'nowait', :equal_to => %w(wait nowait SRC)
 attribute :user, :kind_of => String, :default => 'root', :required => true
 attribute :program, :kind_of => String
