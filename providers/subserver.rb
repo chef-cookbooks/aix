6f5d9f51a9d1c3eca8b4fd5a95a11df64f748b93
vfoucault <vianney.foucault@gmail.com>
2015-05-19 18:19:11 +0200

    Fix regexp & boolean test
    Line 38 : the regex cannot match lines with some extra commands & args for the inetd entry
    Line 39 : obviously, a correct line starting with a # is disabled, so (# == # ) => true which should be false.

--- providers/subserver.rb
+++ providers/subserver.rb
@@ -35,8 +35,8 @@ def load_current_resource
     inetd = ::File.open('/etc/inetd.conf')
     inetd.each_line do |line|
       next if line =~ /^##/  # standard IBM comment
-      if line =~ /^(#?)(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+(.*)$/
-        @current_resource.enabled = ($1 == '#')
+      if line =~ /^(#?)(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s+((.){0,})$/
+        @current_resource.enabled = ($1 != '#')
         # Assume that servicename and protocol are sufficient as a unique identifier
         if @new_resource.servicename == $2 && @new_resource.protocol == $4
           @current_resource.servicename($2)
