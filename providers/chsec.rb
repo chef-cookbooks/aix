de48c4c9c5e3ec7e3ecdabf3f86f0475a288eafc
chmod666org <benoit.creau@chmod666.org>
2015-07-29 21:34:17 +0200

    Adding provider for chsec Adding resource for chsec

---
+++ providers/chsec.rb
@@ -0,0 +1,121 @@
+#
+# Author:: Benoit Creau (<benoit.creau@chmod666.org>)
+# Cookbook Name:: aix
+# Provider:: chsec
+#
+# Copyright:: 2015, Benoit Creau
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+require 'chef/mixin/shell_out'
+
+include Chef::Mixin::ShellOut
+
+use_inline_resources
+
+# support whyrun
+def whyrun_supported?
+  true
+end
+
+# loading current resource 
+def load_current_resource
+  @current_resource = Chef::Resource::AixChsec.new(@new_resource.name)
+
+  # resource exists only if the file exists
+  if ::File.exist?(@current_resource.name)
+    @current_resource.exists = true
+    # check if the stanza exists
+    # if the stanza does not exists the resource does not exists
+    stanza_to_check = @new_resource.stanza
+    if ! ::File.readlines(@new_resource.name).grep(/#{@new_resource.stanza}:/)
+      Chef::Log.info("chsec: no stanza found (#{@new_resource.stanza})")
+      @current_resource.exists = false
+    end
+  else
+    @current_resource.exists = false
+  end
+
+  # we are loading resource this way
+  # a file modified by chsec is like this
+  # usw:
+  #     shells = /bin/sh,/bin/bsh,/bin/csh,/bin/ksh,/bin/tsh,/bin/ksh93,/usr/bin/sh,/usr/bin/bsh,/usr/bin/csh,/usr/bin/ksh,/usr/bin/tsh,/usr/bin/ksh93,/usr/bin/rksh,/usr/bin/rksh93,/usr/sbin/uucp/uucico,/usr/sbin/sliplogin,/usr/sbin/snappd
+  #     maxlogins = 32767
+  #     logintimeout = 60
+  #     maxroles = 8
+  #     auth_type = STD_AUTH
+  # if the resource exists we are loading it
+  if @current_resource.exists
+    # Searching for the stanza
+    found_stanza = false
+    current_attributes = Hash.new
+    ::File.open("#{@new_resource.name}").each_line do |line|
+      if "#{line}".chomp == "#{@new_resource.stanza}:"
+        Chef::Log.info("chsec: found stanza (#{@new_resource.stanza})")
+        found_stanza = true
+        next
+      end
+      # filling the hash table
+      if found_stanza
+        # if the line is empty we skip it
+        if line.chomp.empty?
+          next
+        end
+        line_attribute = line.split("=")
+        # chomp and strip here
+        key = "#{line_attribute[0]}".chomp.strip
+        value = "#{line_attribute[1]}".chomp.strip
+        # to_sym very important
+        current_attributes[key.to_sym] = value
+        Chef::Log.info("chsec: #{@new_resource.stanza} -> [#{key}],[#{value}])")
+      end
+      # if we found the stanza, and we match another stanza found_stanza=0
+      if found_stanza && line =~ /\w:/
+        found_stanza = false
+      end
+    end 
+    # loading the attributes
+    @current_resource.attributes(current_attributes)
+  end
+end
+
+# update action
+action :update do
+  # we update only existing resource
+  if @current_resource.exists
+    chsec_s = "chsec -f #{@new_resource.name} -s #{new_resource.stanza}"
+    change = false
+    # iterating trough the hash table of sec attributes
+    @new_resource.attributes.each do |key,value|
+      # checking if value has to be changed
+      current_attr = @current_resource.attributes[key]
+      new_attr = @new_resource.attributes[key]
+      if "#{@new_resource.attributes[key]}" == "#{@current_resource.attributes[key]}"
+        Chef::Log.info("chsec: value of #{key} already set to #{value} for stanza #{@new_resource.stanza}")
+      else
+        change = true  
+        chsec_s = chsec_s << " -a #{key}=#{@new_resource.attributes[key]}"
+      end
+    end
+    if change
+      # we converge if the is a change to do
+      converge_by("chsec: changing #{@new_resource.name} for stanza #{@new_resource.stanza}") do
+        Chef::Log.info("chsec: command #{chsec_s}")
+        chsec = Mixlib::ShellOut.new(chsec_s)
+        chsec.valid_exit_codes = 0
+        chsec.run_command
+        chsec.error!
+        chsec.error?
+      end
+    end
+  end
+end
---
+++ resources/chsec.rb
@@ -0,0 +1,7 @@
+actions :update
+default_action :update
+attr_accessor :exists
+
+attribute :file_name, :name_attribute => true, :kind_of => String
+attribute :attributes, :kind_of => Hash
+attribute :stanza, :kind_of => String
