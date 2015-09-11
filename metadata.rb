cfe01dbb145a1508da2d959172bdbb45ffcec55f
Tim Smith <tsmith84@gmail.com>
2015-09-10 21:02:58 -0700

    Update URLs

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Custom resources useful for AIX systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-source_url       'https://github.com/opscode-cookbooks/aix' if respond_to?(:source_url)
-issues_url       'https://github.com/opscode-cookbooks/aix/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/chef-cookbooks/aix' if respond_to?(:source_url)
+issues_url       'https://github.com/chef-cookbooks/aix/issues' if respond_to?(:issues_url)
 
 supports 'aix', '>= 6.1'
