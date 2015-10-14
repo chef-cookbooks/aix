b5a99bb6047e042f8e775e958032fde88f33aa1c
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2015-10-14 14:32:26 -0400

    Finish implementation of aix_subsystem resource

--- providers/subsystem.rb
+++ providers/subsystem.rb
@@ -10,26 +10,31 @@ require 'chef/mixin/shell_out'
 
 include Chef::Mixin::ShellOut
 
+# Support whyrun
 def whyrun_supported?
   true
 end
 
 def load_current_resource
   @current_resource = Chef::Resource::AixSubsystem.new(@new_resource.name)
-  @current_resource.enabled = false
+  so = shell_out("lssrc -s #{@new_resource.subsystem_name}")
+  @current_resource.exists = (so.exitstatus == 0)
 end
 
 action :create do
   command = ['-p', new_resource.program]
-  command << ['-u', Etc.getpwuid(new_resource.user)]
+  command << ['-u', new_resource.user]
   command << ['-a', "#{new_resource.arguments.join(' ')}"] if new_resource.arguments
   command << ['-t', new_resource.subsystem_synonym] if new_resource.subsystem_synonym
   command << ['-G', new_resource.subsystem_group] if new_resource.subsystem_group
   command << ['-e', new_resource.standard_error] if new_resource.standard_error
   command << ['-i', new_resource.standard_input] if new_resource.standard_input
   command << ['-o', new_resource.standard_output] if new_resource.standard_output
+  command << ['-S'] if new_resource.use_signals
+  command << ['-f', new_resource.force_stop_signal] if new_resource.use_signals && new_resource.force_stop_signal
+  command << ['-n', new_resource.normal_stop_signal] if new_resource.use_signals && new_resource.normal_stop_signal
 
-  if @current_resource.enabled
+  if @current_resource.exists
     unless @current_resource.subsystem_name == @new_resource.subsystem_name
       command << ['-s', @new_resource.subsystem_name]
     end
@@ -45,7 +50,7 @@ action :create do
 end
 
 action :delete do
-  if @current_resource.enabled
+  if @current_resource.exists
     converge_by('remove subsystem entry') do
       shell_out("rmssys -s #{@current_resource.service_name}")
     end
