847704ee05ffd98e4c3b80a5c3d990b1907f491c
Shahul Hameed <skhajamohid1@bloomberg.net>
2015-10-20 11:11:03 -0400

    Check if subsystem resource is changed

--- providers/subsystem.rb
+++ providers/subsystem.rb
@@ -34,47 +34,77 @@ def load_current_resource
     @current_resource.standard_input(fields[6])
     @current_resource.standard_output(fields[7])
     @current_resource.standard_error(fields[8])
-    @current_resource.standard_error(fields[9])
     #ignore action
-    @current_resource.multiple_instances(fields[11] == '-Q')
-    case fields[12]
+    @current_resource.multiple_instances(fields[10] == '-Q')
+    case fields[11]
     when '-S'
       @current_resource.use_signals(true)
     when '-K'
       @current_resource.use_sockets(true)
     when '-I'
       @current_resource.use_message_queues(true)
-    @current_resource.message_queue_key(fields[13])
-    @current_resource.message_type(fields[14])
-    @current_resource.priority(fields[15])
-    @current_resource.normal_stop_signal(fields[16])
-    @current_resource.force_stop_signal(fields[17])
-    @current_resource.display(fields[18])
-    @current_resource.wait_time(fields[19])
-    @current_resource.group_name(fields[20])
+    @current_resource.message_queue_key(fields[12])
+    @current_resource.message_type(fields[13])
+    @current_resource.priority(fields[14])
+    @current_resource.normal_stop_signal(fields[15])
+    @current_resource.force_stop_signal(fields[16])
+    @current_resource.display(fields[17])
+    @current_resource.wait_time(fields[18])
+    @current_resource.subsystem_group(fields[19])
   end
 end
 
+# Returns true if current_resource is not equal to new_resource
+def resource_changed?
+  return (@new_resource.subsystem_synonym && @current_resource.subsystem_synonym !=  @new_resource.subsystem_synonym)
+  || (@new_resource.arguments && @current_resource.arguments !=  @new_resource.arguments)
+  || (@new_resource.program && @current_resource.program !=  @new_resource.program)
+  || (@new_resource.user && @current_resource.user !=  Etc.getpwnam(@new_resource.user).uid)
+  || (@new_resource.standard_input && @current_resource.standard_input !=  @new_resource.standard_input)
+  || (@new_resource.standard_output && @current_resource.standard_output !=  @new_resource.standard_output)
+  || (@new_resource.standard_error && @current_resource.standard_error !=  @new_resource.standard_error)
+  || (@new_resource.multiple_instances && @current_resource.multiple_instances !=  @new_resource.multiple_instances)
+  || (@new_resource.use_signals && @current_resource.use_signals !=  @new_resource.use_signals)
+  || (@new_resource.use_sockets && @current_resource.use_sockets !=  @new_resource.use_sockets)
+  || (@new_resource.message_queue_key && @current_resource.message_queue_key !=  @new_resource.message_queue_key)
+  || (@new_resource.message_type && @current_resource.message_type !=  @new_resource.message_type)
+  || (@new_resource.priority && @current_resource.priority !=  @new_resource.priority)
+  || (@new_resource.normal_stop_signal && @current_resource.normal_stop_signal !=  @new_resource.normal_stop_signal)
+  || (@new_resource.force_stop_signal && @current_resource.force_stop_signal !=  @new_resource.force_stop_signal)
+  || (@new_resource.display && @current_resource.display !=  @new_resource.display)
+  || (@new_resource.wait_time && @current_resource.wait_time !=  @new_resource.wait_time)
+  || (@new_resource.subsystem_group && @current_resource.subsystem_group !=  @new_resource.subsystem_group)
+end
+
+
 action :create do
-  command = ['-p', new_resource.program]
-  command << ['-u', new_resource.user]
+  command = ['-t', new_resource.subsystem_synonym] if new_resource.subsystem_synonym
   command << ['-a', "\"#{new_resource.arguments.join(' ')}\""] if new_resource.arguments
-  command << ['-t', new_resource.subsystem_synonym] if new_resource.subsystem_synonym
-  command << ['-G', new_resource.subsystem_group] if new_resource.subsystem_group
-  command << ['-e', new_resource.standard_error] if new_resource.standard_error
+  command << ['-p', new_resource.program]
+  command << ['-u', new_resource.user]
   command << ['-i', new_resource.standard_input] if new_resource.standard_input
   command << ['-o', new_resource.standard_output] if new_resource.standard_output
+  command << ['-e', new_resource.standard_error] if new_resource.standard_error
   command << ['-q'] if new_resource.multiple_instances
   command << ['-S'] if new_resource.use_signals
-  command << ['-f', new_resource.force_stop_signal] if new_resource.use_signals && new_resource.force_stop_signal
+  command << ['-K'] if new_resource.use_sockets
+  command << ['-I', new_resource.message_queue_key] if new_resource.use_message_queues && new_resource.message_queue_key
+  command << ['-m', new_resource.message_type] if new_resource.use_message_queues && new_resource.message_type
+  command << ['-E', new_resource.priority] if new_resource.priority
   command << ['-n', new_resource.normal_stop_signal] if new_resource.use_signals && new_resource.normal_stop_signal
+  command << ['-f', new_resource.force_stop_signal] if new_resource.use_signals && new_resource.force_stop_signal
+  command << ['-D'] if new_resource.display
+  command << ['-w', new_resource.wait_time] if new_resource.wait_time
+  command << ['-G', new_resource.subsystem_group] if new_resource.subsystem_group
 
   if @current_resource.exists
     unless @current_resource.subsystem_name == @new_resource.subsystem_name
       command << ['-s', @new_resource.subsystem_name]
     end
-    converge_by('change subsystem entry') do
-      shell_out!(["chssys -s #{@current_resource.subsystem_name}"].concat(command).flatten.join(' '))
+    if resource_changed?
+      converge_by('change subsystem entry') do
+        shell_out!(["chssys -s #{@current_resource.subsystem_name}"].concat(command).flatten.join(' '))
+      end
     end
   else
     converge_by('create subsystem entry') do
