c2a7a62e020708af42da5da21720056ee304a3bb
adejoux <adejoux@krystalia.net>
2016-04-11 13:25:31 +0200

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,37 +24,37 @@ aix_tcpservice 'xntpd' do
   action :enable
 end
 
-aix_chsec "/etc/security/login.cfg" do
-  attributes(:maxlogins => 16000, :maxroles => 7, :shells => "/bin/sh,/usr/bin/ksh")
-  stanza "usw"
+aix_chsec '/etc/security/login.cfg' do
+  attributes(:maxlogins => 16000, :maxroles => 7, :shells => '/bin/sh,/usr/bin/ksh')
+  stanza 'usw'
   action :update
 end
 
-aix_etchosts "test" do
-  ip_address "1.1.1.1"
+aix_etchosts 'test' do
+  ip_address '1.1.1.1'
   action :add
 end
 
-aix_etchosts "test" do
-  ip_address "2.2.2.2"
+aix_etchosts 'test' do
+  ip_address '2.2.2.2'
   action :change
 end
 
-aix_etchosts "test" do
-  new_hostname "test2"
+aix_etchosts 'test' do
+  new_hostname 'test2'
   action :change
 end
 
-aix_etchosts "test" do
-  ip_address "1.1.1.1"
-  aliases ["test2", "test3"]
+aix_etchosts 'test' do
+  ip_address '1.1.1.1'
+  aliases %w(test2 test3)
   action :add
 end
 
-aix_etchosts "test" do
+aix_etchosts 'test' do
   action :delete
 end
 
-aix_etchosts "delete all entries" do
+aix_etchosts 'delete all entries' do
   action :delete_all
 end
