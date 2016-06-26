9a4d86dbb759ebf2306ad03616e06caefcfd0661
chefworkstation <chef@s00vl9948254.fr.net.intra>
2016-06-26 19:26:23 +0200

    Revert "Adding -U to chdev (hot_change parameter)"
    This reverts commit a2e077c79fa31a65eae0d86fc8f4e49cbc40454f.

--- resources/chdev.rb
+++ resources/chdev.rb
@@ -4,6 +4,4 @@ attr_accessor :exists
 
 attribute :name, name_attribute: true, kind_of: String
 attribute :attributes, kind_of: Hash
-# need_reboot and hot_change cannot be used at the same time
 attribute :need_reboot, kind_of: [TrueClass, FalseClass], default: false
-attribute :hot_change, kind_of: [TrueClass, FalseClass], default: false
