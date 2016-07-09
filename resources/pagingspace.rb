ecc7e1675997ec32a45d4d6d18540beb126a96d0
Benoit Creau <chefdev@devplace>
2016-07-10 01:34:44 +0200

    adding pagingspace provider

---
+++ resources/pagingspace.rb
@@ -0,0 +1,14 @@
+actions :change, :remove, :create
+default_action :change
+attr_accessor :exists
+
+# example of lsps -ca
+# name:Pvname:Vgname:Size:Used:Active:Auto:Type:Chksum
+# hd6:hdisk0:rootvg:8:1:y:n:lv:0
+#
+attribute :name, :name_attribute => true, :kind_of => String
+attribute :pvname, :kind_of => String
+attribute :vgname, :kind_of => String
+attribute :size, :kind_of => Fixnum
+attribute :active, :kind_of => [TrueClass, FalseClass], :default => true
+attribute :auto, :kind_of => [TrueClass, FalseClass], :default => true
