f4c0ef431cb23d9f7a145142ca9058d8f2306be1
Benoit Creau <benoit.creau@chmod666.org>
2016-07-10 15:38:29 +0200

    Correcting error in pagingspace resource

--- resources/pagingspace.rb
+++ resources/pagingspace.rb
@@ -8,6 +8,7 @@ attr_accessor :exists
 
 attribute :name, :name_attribute => true, :kind_of => String
 attribute :vgname, :kind_of => String
+attribute :pvname, :kind_of => String
 attribute :size, :kind_of => Fixnum
 attribute :active, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :auto, :kind_of => [TrueClass, FalseClass], :default => true
