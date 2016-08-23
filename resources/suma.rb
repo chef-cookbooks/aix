1b953bb322f8d7b55d6a68133d99fc59b027e50a
jhurstel <hursteljerome@yahoo.fr>
2016-08-23 17:52:48 +0200

    suma custom resource (get ML level, find lowest ML level, preview + download)

--- resources/suma.rb
+++ resources/suma.rb
@@ -16,88 +16,89 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-property :name, String, name_property: true
-property :rq_type, String, default: 'Latest'
-property :rq_name, String
-property :dl_target, String
-property :filter_ml, String
-property :filter_dir, String
-
-# suma reminder
-# suma -c [ -a Field=Value ]...
-# suma -x [ -a Field=Value ]...
-# suma -s CronSched [ -a Field=Value ]...
-# suma -d TaskID
+property :desc, [String,nil], name_property: true
+property :name, String
+property :location, String, default: "/usr/sys/inst.images"
+property :targets, String
 
 load_current_value do
-
 end
-	
+
 action :download do
-  # Example of suma 
-  # suma -x -a Action=Download -a RqType=Latest -a DLTarget=/home/toto -a FilterML=6100-01 -a FilterDir=/usr/sys/inst.images
-  # suma -x -a Action=Download -a RqType=SP -a RqName=6100-01-08 -a DLTarget=/home/toto -a FilterML=6100-01 -a FilterDir=/usr/sys/inst.images
-  # suma -x -a Action=Download -a RqType=TL -a RqName=6100-02 -a DLTarget=/home/toto -a FilterML=6100-01 -a FilterDir=/usr/sys/inst.images
-  
-  suma_s = 'suma -x'
 
-  # getting display name
-  suma_s = suma_s << ' -a DisplayName="' << name << '"'
-  
-  # getting rq_type or use default (allowed: Latest, SP, TL)
-  suma_s = suma_s << ' -a RqType=' << rq_type
+  Chef::Log.info("name=#{name}")
+  Chef::Log.info("location=#{location}")
+  Chef::Log.info("targets=#{targets}")
 
-  # getting rq_name
-  if property_is_set?(:rq_name)
-    unless rq_type == 'Latest'
-      suma_s = suma_s << ' -a RqName=' << rq_name
+  machines=targets.split(',')
+  Chef::Log.info("machines=#{machines}")
+
+  # find lowest ML level by comparing each machine's oslevel
+  filter_ml='7200-00'
+  filter_ml.delete!('-')
+  machines.each do |machine|
+
+    new_filter_ml = shell_out!("/usr/lpp/bos.sysmgt/nim/methods/c_rsh #{machine} \"/usr/bin/oslevel -r\"").stdout.chomp!
+    Chef::Log.info("Obtained ML level for machine #{machine}: #{new_filter_ml}")
+
+	new_filter_ml.delete!('-')
+	old_filter_ml=new_filter_ml
+	if new_filter_ml <= old_filter_ml
+	  filter_ml=new_filter_ml
+	end
+
+  end
+  filter_ml.insert(4, '-')
+  Chef::Log.info("Lowest ML level is: #{filter_ml}")
+
+  # create location if it does not exist
+  dl_target="#{location}/#{name}-lpp_source"
+  Chef::Log.info("Checking location #{dl_target}...")
+  directory dl_target do
+    recursive true
+    action :create
+  end
+
+  suma_s="suma -x -a DisplayName=#{desc} -a RqType=SP -a RqName=#{name} -a DLTarget=#{dl_target} -a FilterML=#{filter_ml}"
+  res_name="#{name}-lpp_source"
+
+  # suma preview
+  dl=0
+  Chef::Log.info("Preview operation...")
+  so=shell_out("#{suma_s} -a Action=Preview 2>&1")
+  #raise "no: #{suma_s} -a Action=Preview failed" if so.exitstatus != 0
+  if so.error?
+	Chef::Log.info("suma returns an error...")
+    need=shell_out!("echo \"#{so.stdout}\" | grep \"0500-035 No fixes match your query.\"")
+    if need.error?
+	  Chef::Log.info("Other suma error")
+	else
+	  Chef::Log.info("Suma error: No fixes match your query")
+	end
+  else
+	dl=shell_out("echo \"#{so.stdout}\" | grep \"Total bytes of updates downloaded:\" | cut -d' ' -f6").stdout
+	if dl == 0
+	  Chef::Log.info("Nothing to download")
+	else
+      Chef::Log.info("Something to download")
     end
   end
 
-  # getting dl_target
-  if property_is_set?(:dl_target)
-    # create if not already created
-    directory dl_target do
-      recursive true
-	  action :create
+  unless dl == 0 
+    # suma download
+    converge_by("suma download operation: \"#{suma_s}\"") do
+      Chef::Log.info("Download fixes...")
+      so=shell_out("#{suma_s} -a Action=Download 2>&1")
     end
 
-    suma_s = suma_s << ' -a DLTarget=' << dl_target 
+    # nim define
+    Chef::Log.info("Define #{res_name} ...")
+    so=shell_out("nim -o define -t lpp_source -a server=master -a location=#{dl_target} #{res_name}")
   end
 
-  # getting filter_ml
-  if property_is_set?(:filter_ml)
-    suma_s = suma_s << ' -a FilterML=' << filter_ml
-  end
-
-  # getting filter_dir
-  if property_is_set?(:filter_dir)
-    suma_s = suma_s << ' -a FilterDir=' << filter_dir
-  end
-
-  
-  #available_space=shell_out("df -g #{dl_target} | tail -n 1 | tr -s ' ' | cut -d' ' -f3").stdout
-  #needed_space=shell_out("echo \"scale=2; `#{suma_s} -a Action=Preview | grep \\"Total bytes of updates downloaded:\\" | cut -d' ' -f6`/1024/1024/1024\" | bc").stdout
-  
-  begin
-    # command to run is build here
-    so = shell_out!("#{suma_s} -a Action=Preview")
-	#puts so
-	dl = shell_out("echo \"#{so.stdout}\" | grep \"Total bytes of updates downloaded:\" | cut -d' ' -f6").stdout
-	
-	unless dl == 0
-	  converge_by("suma download operation: \"#{suma_s}\"") do
-        suma = Mixlib::ShellOut.new(suma_s)
-        suma.valid_exit_codes = 0
-        suma.run_command
-        suma.error!
-        suma.error?
-	  end
-    end
-  
-  rescue Mixlib::ShellOut::ShellCommandFailed => e
-    Chef::Log.fatal(e.message)
-  end
+  #rescue Mixlib::ShellOut::ShellCommandFailed => e
+  #  Chef::Log.fatal(e.message)
+  #end
   
   # converge here
   #unless do_not_converge
