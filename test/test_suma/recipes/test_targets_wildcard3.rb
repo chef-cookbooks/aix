badfb64fe46f288a2643540271d67c78e5ac4e7a
cyril-atos <cyril.bouhallier@atos.net>
2016-10-11 15:54:22 +0200

    Added files
    Signed-off-by: Jerome Hurstel <jerome.hurstel@atos.net>

---
+++ test/test_suma/recipes/test_targets_wildcard3.rb
@@ -0,0 +1,20 @@
+# Expected values
+# return code : 0
+# exception : nil
+# suma directory : […]
+# suma metadata : […]
+# suma preview : […]
+# suma download : […]
+# nim define : […]
+
+
+node.default['nim']['clients'] = { 'client1' => { 'oslevel' => '7100-02-01' },
+                                   'client2' => { 'oslevel' => '7100-03-01' },
+                                   'client3' => { 'oslevel' => '7100-04-01' } }
+
+aix_suma '31. Valid client list with wildcard' do
+  oslevel   '7100-02-02'
+  location  '/tmp/img.source/targets_wd3/'
+  targets   'client*'
+  action    :download
+end
---
+++ test/test_suma/recipes/test_targets_wildcard4.rb
@@ -0,0 +1,20 @@
+# Expected values
+# return code : 0
+# exception : nil
+# suma directory : […]
+# suma metadata : […]
+# suma preview : […]
+# suma download : […]
+# nim define : […]
+
+
+node.default['nim']['clients'] = { 'client1' => { 'oslevel' => '7100-02-01' },
+                                   'client2' => { 'oslevel' => '7100-03-01' },
+                                   'client3' => { 'oslevel' => '7100-04-01' } }
+
+aix_suma 'Valid client list with wildcard' do
+  oslevel   '7100-02-02'
+  location  '/tmp/img.source/targets_wd4/'
+  targets   '*'
+  action    :download
+end
