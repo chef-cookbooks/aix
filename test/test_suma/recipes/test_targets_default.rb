9c2a18f13ed7d89ae9a8f3159db5b26ac680b1e9
atos-hurstel <jerome.hurstel@atos.net>
2016-10-17 18:43:21 +0200

    error suma metadata targets property required Signed-off-by: Jerome Hurstel <jerome.hurstel@atos.net>

--- test/test_suma/recipes/test_targets_default.rb
+++ test/test_suma/recipes/test_targets_default.rb
@@ -1,6 +1,6 @@
 # Expected values
 # return code : 1
-# exception : AIX::PatchMgmt::InvalidTargetsProperty
+# exception : Chef::Exceptions::ValidationFailed
 
 node.default['nim'] = { 'clients' => { 'client1' => { 'oslevel' => '7100-02-01-1316' },
                                        'client2' => { 'oslevel' => '7100-03-01-1316' },
