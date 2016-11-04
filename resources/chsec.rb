42186515f001c417bf5219fce7c5bafdc0eaf488
jchalfan@us.ibm.com <jchalfan@us.ibm.com>
2016-11-04 15:17:58 -0500

    should be required
    Signed-off-by: Jeremy Chalfant <jchalfan@us.ibm.com>

--- resources/chsec.rb
+++ resources/chsec.rb
@@ -16,8 +16,7 @@
 
 actions :update
 default_action :update
-attr_accessor :exists
 
 attribute :file_name, name_attribute: true, kind_of: String
-attribute :attributes, kind_of: Hash
-attribute :stanza, kind_of: String
+attribute :attributes, kind_of: Hash, required: true
+attribute :stanza, kind_of: String, required: true
