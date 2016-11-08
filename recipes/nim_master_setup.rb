8902426ab7ecefee23cb620bf527b00ac7f135a4
Patrice-Atos <patrice.jacquin@atos.net>
2016-11-08 13:46:26 +0100

    New recipe to setup a nim master
    Signed-off-by: Jerome Hurstel <jerome.hurstel@atos.net>

--- recipes/nim_master_setup.rb
+++ recipes/nim_master_setup.rb
@@ -1,30 +1,69 @@
 # recipe example setup nim master
-# Assume that the 'nim_server' is the hostname of the nim server
-# Assume that the level of lpp_source required for installing bos.sysmgmt.nim.master
-# has already been allocated by nim server and fs exported.
-# > nim -o allocate -a lpp_source=<lpp_source> <new_nim_server>
-# > exportfs
+# Assume that the 'nim_server' is the hostname of the current nim server
+# and the 'new_nim_server' is the hostname of the nim master to setup
+nim_server = 'pollux6'
+lpp_source = '1626A_72D_lpp'
+new_nim_server = 'pollux6c'
 
-nim_server = 'fattony01'
-lpp_source = '7143lpp_res'
+nim_script_res_name  = "chef_nim_setup_script_#{new_nim_server}"
+nim_script_file_name = "/tmp/#{nim_script_res_name}.sh"
+mount_point = "/mnt_chef_master_setup"
 
-# mounting /mnt
-mount '/mnt' do
-  device "#{nim_server}:/export/nim/lpp_source/#{lpp_source}"
-  fstype 'nfs'
-  action :mount
+cmd = Mixlib::ShellOut.new("lsnim -l #{lpp_source} | grep location | awk '{ print $NF }'")
+cmd.run_command
+cmd.valid_exit_codes = 0
+if cmd.error?
+  ;
+end
+nim_res_path = cmd.stdout.chomp
+
+# Create the script to set the new nim master
+file "#{nim_script_file_name}" do
+  content "#!/bin/ksh\nexport LANG=C\nmkdir #{mount_point}\nmount #{nim_server}:#{nim_res_path} #{mount_point}\nnim_master_setup -a mk_resource=no -B -a device=#{mount_point}\numount #{mount_point}\nrmdir #{mount_point}\n"
+  mode '0777'
+  owner 'root'
+  action :create
 end
 
-# setup nim master
-aix_nim 'Install nim package' do
-  device '/mnt'
-  action :master_setup
+# Define the script nim resource
+execute 'def_script_res' do
+  command "nim -o define -t script -a location=#{nim_script_file_name} -a server=master #{nim_script_res_name}"
+  action :run
 end
 
-# unmount /mnt
-mount '/mnt' do
-  action :umount
+# Allocate the required lpp_source
+aix_nim 'Allocate lpp_source' do
+  lpp_source "#{lpp_source}"
+  targets "#{new_nim_server}"
+  action :allocate
 end
 
-# Do not forget to deallocate the resource on the old nim server
-# > nim -o deallocate -a lpp_source=<lpp_source> <new_nim_server>
+# Export the resources
+execute 'export_res' do
+  command "exportfs"
+  action :run
+end
+
+# Setup the nim master
+execute 'setup_master' do
+  command "nim -o cust -a script=#{nim_script_res_name} -a async=no #{new_nim_server}"
+  action :run
+end
+
+# Deallocate the resource on the old nim server
+aix_nim 'Deallocate lpp_source' do
+  lpp_source "#{lpp_source}"
+  targets "#{new_nim_server}"
+  action :deallocate
+end
+
+# Remove the script resource
+execute 'rem_script_res' do
+  command "nim -o remove #{nim_script_res_name}"
+  action :run
+end
+
+# Remove the script file
+file "#{nim_script_file_name}" do
+  action :delete
+end
