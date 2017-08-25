c20b58b186bc48acc6d7bd7f9b341b62a9d5fe50
ponceta-jm <alain.poncet@atos.net>
2017-08-25 13:18:01 +0200

    efixes installation continue even if an efix install fails due to files lock add otion: install all efix from a repository
    Signed-off-by: ponceta-jm <alain.poncet@atos.net>

--- providers/fixes.rb
+++ providers/fixes.rb
@@ -1,9 +1,5 @@
 #
-# Author:: Benoit Creau (<benoit.creau@chmod666.org>)
-# Cookbook Name:: aix
-# Provider:: fixes
-#
-# Copyright:: 2015, Benoit Creau
+# Copyright:: 2015-2017, Benoit Creau <benoit.creau@chmod666.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,9 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-require 'chef/mixin/shell_out'
-
-include Chef::Mixin::ShellOut
+#
 
 use_inline_resources
 
@@ -31,10 +25,9 @@ end
 def load_current_resource
   @current_resource = Chef::Resource::AixFixes.new(@new_resource.name)
 
-  emgr = Mixlib::ShellOut.new('emgr -l')
-  emgr.run_command
+  emgr = shell_out('/usr/sbin/emgr -l')
+  Chef::Log.fatal('emgr: error while running emgr') if emgr.error?
   emgr.error!
-  Chef::Log.fatal('emgr: error while running emgr') unless emgr.exitstatus
 
   # resource does not exists if there are no efix installed on the system
   if emgr.stdout == 'There is no efix data on this system'
@@ -55,31 +48,30 @@ end
 
 action :remove do
   if @current_resource.exists
-    if @new_resource.fixes[0] == 'all'
-      @current_resource.fixes.each do |fix|
-        converge_by("emgr: removing fix #{fix}") do
-          remove_emgr = Mixlib::ShellOut.new("emgr -r -L #{fix}")
-          remove_emgr.run_command
-          remove_emgr.error!
-          unless remove_emgr.exitstatus
-            Chef::Log.fatal("emgr: error while trying to removing fix #{fix}")
-          end
-        end
-      end
+    if @new_resource.fixes[0].downcase == 'all'
+      fixes_to_remove = @current_resource.fixes
     else
-      @new_resource.fixes.each do |fix|
-        converge = false
+      fixes_to_remove = @new_resource.fixes
+    end
+    Chef::Log.info("efixes requested to be removed: #{fixes_to_remove}")
+    fixes_to_remove.each do |fix|
+      converge = false
+      unless @new_resource.fixes[0].downcase == 'all'
         @current_resource.fixes.each do |i_fix|
           converge = true if i_fix.include? fix
-          next unless converge
-          converge_by("emgr: removing fix #{fix}") do
-            remove_emgr = Mixlib::ShellOut.new("emgr -r -L #{fix}")
-            remove_emgr.run_command
-            remove_emgr.error!
-            unless remove_emgr.exitstatus
-              Chef::Log.fatal("emgr: error while trying to removing fix #{fix}")
-            end
-          end
+        end
+        unless converge
+          Chef::Log.error("efix: #{fix} is not installed.")
+          next
+        end
+      end
+      converge_by("emgr: fix #{fix} removed.\n") do
+        Chef::Log.debug("emgr: removing efix #{fix} using command: /usr/sbin/emgr -r -L #{fix}")
+        remove_emgr = shell_out("/usr/sbin/emgr -r -L #{fix}")
+        if remove_emgr.error?
+          Chef::Log.fatal("emgr: error while trying to removing fix #{fix}")
+          Chef::Log.error(remove_emgr.stdout)
+          remove_emgr.error!
         end
       end
     end
@@ -88,22 +80,37 @@ end
 
 action :install do
   fix_directory = @new_resource.directory
-  @new_resource.fixes.each do |fix|
-    emgr_install_string = 'emgr -X -e ' << fix_directory << '/' << fix
-    converge = true
-    if @current_resource.exists
-      @current_resource.fixes.each do |i_fix|
-        converge = false if fix.include? i_fix
+  if Dir.exist?(fix_directory)
+    Dir.chdir(fix_directory)
+    if @new_resource.fixes[0].downcase == 'all'
+      packages = Dir.glob("*.epkg.Z")
+    else
+      packages = @new_resource.fixes
+    end
+    packages.sort! {|x, y| y <=> x}
+
+    packages.each do |fix|
+      emgr_install_string = '/usr/sbin/emgr -p -e ' << fix_directory << '/' << fix
+      Chef::Log.debug("emgr: installing in preview mode efix #{fix} using command: #{emgr_install_string}")
+      install_emgr = shell_out(emgr_install_string)
+      if install_emgr.error?
+        Chef::Log.error("emgr: error during preview install, fix package: #{fix} skiped.")
+        Chef::Log.info(install_emgr.stdout)
+        next
+      end
+
+      emgr_install_string = '/usr/sbin/emgr -X -e ' << fix_directory << '/' << fix
+      converge_by("emgr: fix #{fix} installed.\n") do
+        Chef::Log.debug("emgr: installing efix #{fix} using command: #{emgr_install_string}")
+        install_emgr = shell_out(emgr_install_string)
+        if install_emgr.error?
+          Chef::Log.fatal("emgr: error while trying to install fix #{fix}")
+          Chef::Log.error(install_emgr.stdout)
+          install_emgr.error!
+        end
       end
     end
-    next unless converge
-    converge_by("emgr: installing fix #{fix}") do
-      install_emgr = Mixlib::ShellOut.new(emgr_install_string)
-      install_emgr.run_command
-      install_emgr.error!
-      unless install_emgr.exitstatus
-        Chef::Log.fatal("emgr: error while trying to install fix #{fix}")
-      end
-    end
+  else
+    Chef::Log.fatal("dir: #{fix_directory} does not exist")
   end
 end
