fa0a00c4d4f08aa55d839b1f7baf03b868936d68
Þórhallur Sverrisson <thorhallur.sverrisson@rb.is>
2017-09-05 14:05:16 +0000

    Fixing foodcritic errors FC069 and FC078 by adding dash to license name
    Signed-off-by: Thorhallur Sverrisson <toti@toti.is>

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,7 @@
 name             'aix'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Custom resources useful for AIX systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.1'
