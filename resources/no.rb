eb43061b6895fb1429de6acbff4c97c325b44c90
michael.sgarbossa <michael.sgarbossa@cvshealth.com>
2017-11-08 17:11:29 -0700

    original license
    Signed-off-by: michael.sgarbossa <michael.sgarbossa@cvshealth.com>

--- resources/no.rb
+++ resources/no.rb
@@ -1,5 +1,5 @@
 #
-# Copyright:: 2014-2016, Chef Software, Inc.
+# Copyright:: 2015-2016, Benoit Creau <benoit.creau@chmod666.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
