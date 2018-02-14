chef-cookbooks/aix

commit 6a1b6261be514b40a7e624d21b1913cbd0d73b45

Fix rubocop finding
Signed-off-by: Navdeep Dhaliwal <34124607+navdeepd@users.noreply.github.com>

resources/chsec.rb
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-property :file_name, String, identity: true, default: lazy { |r| r.name }
+property :file_name, String, identity: true, default: lazy(&:name)
 property :attributes, Hash
 property :stanza, String, desired_state: false
 

commit 246a880bc6feebbfe825ac0439ae947f08bf7c99

Fix previous bug introduced by commit 6a1b6261be514b40a7e624d21b1913cbd0d73b45
Signed-off-by: Navdeep Dhaliwal <34124607+navdeepd@users.noreply.github.com>

resources/chsec.rb
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-property :file_name, String, identity: true, default: lazy(&:name)
+property :file_name, String, name_property: true 
 property :attributes, Hash
 property :stanza, String, desired_state: false
 
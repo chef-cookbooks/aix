246a880bc6feebbfe825ac0439ae947f08bf7c99
Navdeep Dhaliwal <34124607+navdeepd@users.noreply.github.com>
2018-02-14 13:53:21 +1100

    Fix previous bug introduced by commit 6a1b6261be514b40a7e624d21b1913cbd0d73b45
    Signed-off-by: Navdeep Dhaliwal <34124607+navdeepd@users.noreply.github.com>

--- resources/chsec.rb
+++ resources/chsec.rb
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-property :file_name, String, identity: true, default: lazy(&:name)
+property :file_name, String, name_property: true 
 property :attributes, Hash
 property :stanza, String, desired_state: false
 
