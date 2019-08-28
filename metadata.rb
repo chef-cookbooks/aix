cd71146011a97567c1376bdda9d0015213976c3e
Tim Smith <tsmith84@gmail.com>
2019-08-27 20:51:18 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Custom resources useful for AIX systems'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.3.2'
 source_url       'https://github.com/chef-cookbooks/aix'
 issues_url       'https://github.com/chef-cookbooks/aix/issues'
