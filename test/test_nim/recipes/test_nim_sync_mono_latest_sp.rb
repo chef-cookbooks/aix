11ae643c3882429c934c7d6a0c5d23146259b381
Tim Smith <tsmith84@gmail.com>
2019-11-10 17:39:39 -0800

    Cookstyle formatting fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/test_nim/recipes/test_nim_sync_mono_latest_sp.rb
+++ test/test_nim/recipes/test_nim_sync_mono_latest_sp.rb
@@ -8,7 +8,7 @@ node.default['nim'] = { 'master' => { 'oslevel' => '7100-02-01' },
                                            '7100-09-05-0000-lpp_source' => { 'location' => '/tmp/img.source/7100-09-05-0000-lpp_source' },
                                            '7100-10-04-0000-lpp_source' => { 'location' => '/tmp/img.source/7100-10-04-0000-lpp_source' },
                                            '7100-11-04-0000-lpp_source' => { 'location' => '/tmp/img.source/7100-11-04-0000-lpp_source' },
-                                           '7100-11-06-0000-lpp_source' => { 'location' => '/tmp/img.source/7100-11-06-0000-lpp_source' } }	}
+                                           '7100-11-06-0000-lpp_source' => { 'location' => '/tmp/img.source/7100-11-06-0000-lpp_source' } } }
 
 aix_nim 'synchronous update latest sp' do
   lpp_source 'latest_sp'
